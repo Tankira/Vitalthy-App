fd39722103c5e0777a8b22ad9a035857e0083998
Tankira <lunamc.tonyvn@gmail.com>
2024-11-26 12:55:55 +0700

    Add and edit components

---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="container" class="flex bg-gray-200 min-h-screen">
+        <div class="bg-white shadow-2xl m-2 ml-auto p-20 rounded-xl w-[38%] text-center side">
+            <h1 class="font-bold text-4xl">Chào mừng!</h1>
+            <p class="mt-4">Đăng nhập tài khoản của bạn để truy cập vào Vitalthy</p>
+            <form class="flex">
+                <input type="text" class="outline-gray-500 peer">
+                <label for="username" class="absolute">Tên đăng nhập</label>
+            </form>
+        </div>
+    </div>
+</template>
