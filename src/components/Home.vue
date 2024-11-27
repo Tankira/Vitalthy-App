816ec7f8026fa249549192b477f2af0d2507de4a
Tankira <lunamc.tonyvn@gmail.com>
2024-11-27 19:56:43 +0700

    Rewrite

--- src/components/Home.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <p>adu</p>
-</template>
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router/router';
-
-import { initializeApp } from "firebase/app";
-createApp(App)
-    .use(router)
-    .mount('#app')
