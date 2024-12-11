0574c6f0541d125e83b981967a57fea4c22d54ad
Tankira <lunamc.tonyvn@gmail.com>
2024-12-11 14:15:08 +0700

    Fix currentUserStatus

--- src/uses/auth.js
+++ src/uses/auth.js
@@ -1,9 +1,7 @@
-import { ref } from 'vue'
-
 // Current user status
 export const getCurrentUser = async () => {
     const { getAuth, onAuthStateChanged } = await import('firebase/auth')
-    new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const cancelListener = onAuthStateChanged(
             getAuth(),
             (user) => {
@@ -16,13 +14,4 @@ export const getCurrentUser = async () => {
             }
         )
     })
-}
-
-// Login
-export const loginUser = async (email, password) => {
-    const error = ref(null)
-    const {getAuth, signInWithEmailAndPassword } = await import('firebase/auth')
-    await signInWithEmailAndPassword(getAuth(), email, password)
-    .catch((err) => { error.value = err.code })
-    return {error}
 }
